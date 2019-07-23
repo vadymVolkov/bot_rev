383dc7d7b8a144fda79074cf48830fe759ecbf3f
Vadym Volkov <vadym@MacBook-Pro-Vadym.local>
2019-07-24 00:34:25 +0300

    changes in bot

--- reply_keyboard_markups.py
+++ reply_keyboard_markups.py
@@ -12,3 +12,11 @@ class Keyboard:
         self.bot.send_message(user_id,
                               'Привет! Я бот который будет присылать тебе отзывы! Если те еще не зарегестрировался, запусти команду /reg !',
                               reply_markup=user_markup)
+
+    def reg(self, message):
+        user_id = message.from_user.id
+        user_markup = telebot.types.ReplyKeyboardMarkup(True, False)
+        user_markup.row('/start', '/reg')
+        self.bot.send_message(user_id,
+                              'Спасибо за регестрацию! Теперь ты будешь получать от меня отзывы!',
+                              reply_markup=user_markup)
