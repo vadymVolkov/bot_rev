0adf0bc09487cc625295cf0f684554fa6aee6922
Vadym Volkov <vadym@MacBook-Pro-Vadym.local>
2019-07-24 11:19:19 +0300

    last change

--- reply_keyboard_markups.py
+++ reply_keyboard_markups.py
@@ -19,7 +19,7 @@ class Keyboard:
         user_markup = telebot.types.ReplyKeyboardMarkup(True, False)
         user_markup.row('/start', '/reg')
         self.bot.send_message(user_id,
-                              'Спасибо за регестрацию! Твой ID ' + user_id + '. \n Теперь ты будешь получать от меня '
+                              'Спасибо за регестрацию! Твой ID ' + str(user_id) + '. \n Теперь ты будешь получать от меня '
                                                                              'отзывы! Попробуй отсканировать этот QR '
                                                                              'http://167.71.59.173/qr или перейди по '
                                                                              'ссылке http://167.71.59.173',
